b3769e80b75ddf3c1d3c2324078818b5b8389a78
Sam Bunger <sam.bunger@gmail.com>
2021-01-12 00:37:12 -0500

    memory performant rendering system

--- components/editor/pdfRenderer/PDFRenderer.tsx
+++ components/editor/pdfRenderer/PDFRenderer.tsx
@@ -2,20 +2,40 @@ import { AssignmentReturnOutlined } from '@material-ui/icons';
 import React from 'react';
 import { listener, trigger } from '../../globalEvents/events';
 
+const DEFAULT_HEIGHT = 1600;
+const DEFAULT_WIDTH = 800;
+const MAX_LOADED = 10;
+
+type ScanDirection = 'NONE' | 'UP' | 'DOWN';
+
+interface Viewport {
+  height: number;
+  width: number;
+}
+
+interface BasePage {
+  pageNumber: number;
+  viewport?: Viewport;
+  divRef: React.RefObject<any>;
+  canvasRef: React.RefObject<any>;
+}
+
+interface LoadedPage extends BasePage {
+  page: any;
+  loaded: true;
+}
+
+interface UnloadedPage extends BasePage {
+  loaded: false;
+}
+
+type Page = LoadedPage | UnloadedPage;
+
 interface PDFRendererProps {
   pdfUrl: string; 
   pageNumber: number;
   scale: number;
   position: number;
-}
-
-interface Page {
-  page: any;
-  viewport: any;
-  pageNumber: number;
-  divRef: React.RefObject<any>;
-  canvasRef: React.RefObject<any>;
-  rendered: boolean;
 }
 
 type PDFRendererState = {
@@ -25,6 +45,7 @@ type PDFRendererState = {
   viewport?: any;
   textContent?: any;
   pages: Page[];
+  defaultViewport: Viewport;
   currentPage: number;
 };
 
@@ -36,13 +57,14 @@ export default class PDFRenderer extends React.Component<
 > {
   state: PDFRendererState;
   pdfjsLib!: any;
-  renderPointer: number;
   isRendering: boolean;
   lastViewed: number;
   numPages: number;
-  lastRenderBatch: number[];
-  currentRenderBatch: number[];
-  needsReRender: 'NO' | 'YES' | 'FLUSH';
+  previousHeight: number;
+  renderedList: number[];
+  loadedQueue: number[];
+  renderQueue: number[];
+  isRendering: boolean;
 
   constructor(props: PDFRendererProps) {
     super(props);
@@ -53,15 +75,19 @@ export default class PDFRenderer extends React.Component<
       viewport: undefined,
       textContent: undefined,
       pages: [],
-      currentPage: 1
+      defaultViewport: {
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT
+      },
+      currentPage: 0
     };
-    this.renderPointer = 0;
     this.isRendering = false;
-    this.lastViewed = 0;
     this.numPages = 0;
-    this.currentRenderBatch = [];
-    this.lastRenderBatch = [];
-    this.needsReRender = 'NO';
+    this.previousHeight = this.props.position;
+    this.renderedList = [];
+    this.loadedQueue = [];
+    this.renderQueue = [];
+    this.isRendering = false;
   }
 
   componentDidMount = () => {
@@ -76,94 +102,184 @@ export default class PDFRenderer extends React.Component<
     }
     if (this.props.scale !== prevProps.scale) {
       this.rescale();
-    } else {
-      this.renderDocument(false);
+    }
+    if (this.computeCurrentPage()) {
+      this.update();
     }
   };
 
-  renderDocument = (flush: boolean) => {
-    if (this.isRendering) {
-      if ((this.needsReRender === 'NO' || this.needsReRender === 'YES') && flush) {
-        this.needsReRender = 'FLUSH';
-      } else if (this.needsReRender === 'NO'){
-        if (flush) this.needsReRender = 'FLUSH';
-        else this.needsReRender = 'YES';
+  computeCurrentPage = () => {
+    if (this.props.position === this.previousHeight) return false;
+    const searchDown = this.props.position < this.previousHeight;
+    for (let i = this.state.currentPage; i < this.state.pages.length && i >= 0; searchDown ? i++ : i--) {
+      if (!this.state.pages[i]) break;
+      if (this.isCurrentPage(this.state.pages[i].divRef)) {
+        if (this.state.currentPage === i) break;
+        trigger('PAGE_CHANGE', { page: i+1 });
+        this.setState({ currentPage: i });
+        break;
       }
-      return; //Another page is being rendered
     }
-    this.needsReRender = 'NO';
-    this.isRendering = true;
-    this.currentRenderBatch = [];
-    if (flush) this.lastRenderBatch = [];
-    this.renderPage(0, false);
+    this.previousHeight = this.props.position;
+    return true;
   }
 
-  renderPage = (i: number, oneRendered: boolean) => {
-    if (this.needsReRender !== 'NO') {
-      if (!oneRendered) {
-        if (this.needsReRender === 'FLUSH') this.lastRenderBatch = [];
-        this.isRendering = false; 
-        this.renderDocument(this.needsReRender === 'FLUSH');
-        return;
-      } else {
-        this.needsReRender = 'NO';
-      }
-    }
+  isCurrentPage = (pageRef: React.RefObject<any>) => {
+    const rect = pageRef.current.getBoundingClientRect();
+    const screenCenter = (window.innerHeight || document.documentElement.clientHeight)/2;
+    return (
+      rect.bottom >= screenCenter &&
+      rect.top <= screenCenter
+    );
+  }
 
-    if (!this.state.pages[i]) {
-      this.isRendering = false;
-      return;
-    }
-
-    const page = this.state.pages[i];
-    if (this.isViewable(page.divRef)) {
-      if (!this.lastRenderBatch.includes(i)) {
-        const context = page.canvasRef.current.getContext('2d');
-        const renderContext = {
-          canvasContext: context,
-          viewport: page.viewport
-        };
-        //Render the page
-        page.page.render(renderContext).promise
-        .then(() => {
-          this.currentRenderBatch.push(i);
-          this.renderPage(i+1, true);
-        })
-        .catch(error => {
-          this.renderPage(i+1, true);
+  update = () => {
+    const renderPromises = [
+      new Promise<void>((resolve) => {
+        this.updatePage(this.state.currentPage, 'NONE', () => {
+          resolve();
         });
-      } else {
-        this.currentRenderBatch.push(i);
-      }
-    } else if (oneRendered){
-      if (this.currentRenderBatch.length !== 0) 
-        this.lastRenderBatch = this.currentRenderBatch;
-      
-      this.isRendering = false;
+      }),
+      new Promise<void>((resolve) => {
+        this.updatePage(this.state.currentPage, 'UP', () => {
+          resolve();
+        });
+      }),
+      new Promise<void>((resolve) => {
+        this.updatePage(this.state.currentPage, 'DOWN', () => {
+          resolve();
+        });
+      })
+    ];
+    
+    Promise.all(renderPromises).then(() => {
+      this.setState({
+        pages: this.state.pages
+      }, () => {
+        this.renderLock();
+      });
+    });
+  }
 
-      return;
+  updatePage = (index: number, direction: ScanDirection, done: () => void) => {
+
+    const isDone = () => {
+      if (direction === 'NONE') done();
+      else if (direction === 'DOWN') {
+        this.updatePage(index - 1, direction, done);
+      } else if (direction === 'UP') {
+        this.updatePage(index + 1, direction, done);
+      } 
+    };
+
+    const page = this.state.pages[index];
+    
+    if (!page || !this.isViewable(page.divRef)) return done();
+
+    if (this.renderedList.includes(index)) return isDone();
+
+    if (page.loaded) {
+      this.queueForRender(index);
+      isDone();
+    } else {
+      this.grabPageData(index, () => {
+        this.queueForRender(index);
+        isDone();
+      });
     }
-    this.renderPage(i+1, false);
+  }
+
+  queueForRender = (index: number) => {
+    if (!this.renderQueue.includes(index)) {
+      this.renderQueue.push(index);
+    }
+  }
+
+  renderLock = () => {
+    if (this.isRendering) return;
+    this.isRendering = true;
+    this.renderPages(() => {
+      this.isRendering = false;
+    });
+  }
+
+  renderPages = (done: () => void) => {
+
+    if (this.renderQueue.length === 0) return done();
+    
+    const index = this.renderQueue.shift();
+    const page = this.state.pages[index];
+
+    if (!page.loaded) throw new Error('Page has not yet loaded!'); 
+
+    const context = page.canvasRef.current.getContext('2d');
+    const renderContext = {
+      canvasContext: context,
+      viewport: page.viewport
+    };
+    //Render the page
+    page.page.render(renderContext).promise
+    .then(() => {
+      this.renderedList.push(index);
+      this.renderPages(done);
+    })
+    .catch(error => {
+      console.error(error);
+      this.renderPages(done);
+    });
+  }
+
+  grabPageData = (index: number, cb: () => void) => {
+    this.state.pdf.getPage(index+1).then((page) => {
+      //Load in contents of new page
+      const currentPage: LoadedPage = this.state.pages[index] as LoadedPage;
+      const viewport = page.getViewport({ scale: this.props.scale });
+      currentPage.loaded = true;
+      currentPage.page = page;
+      currentPage.viewport = viewport;
+      
+      //Add new page to queue
+      const queueIndex = this.loadedQueue.indexOf(index);
+      if (queueIndex > -1) {
+        this.loadedQueue.slice(queueIndex);
+        this.loadedQueue.push(index);
+      }
+
+      //Remove from queue if too many loaded pages
+      if (this.loadedQueue.length > MAX_LOADED) {
+        const removeIndex = this.loadedQueue.shift();
+
+        const page: LoadedPage = this.state.pages[removeIndex] as LoadedPage;
+        delete page.page;
+
+        const pageSetLoadedFalse = this.state.pages[removeIndex];
+        pageSetLoadedFalse.loaded = false;
+      }
+
+      cb();
+    });
   }
 
   isViewable = (pageRef: React.RefObject<any>) => {
     const rect = pageRef.current.getBoundingClientRect();
     return !(
-      rect.bottom <= 0 ||
+      rect.bottom <= -500 ||
       rect.right <= 0 ||
-      rect.top >= (window.innerHeight || document.documentElement.clientHeight) ||
+      rect.top >= (window.innerHeight || document.documentElement.clientHeight) + 500 ||
       rect.left >= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
 
   rescale = () => {
     for (const page of this.state.pages) {
-      page.viewport = page.page.getViewport({ scale: this.props.scale });
+      if (page.loaded)
+        page.viewport = page.page.getViewport({ scale: this.props.scale });
     }
     this.setState({
       pages: this.state.pages
     }, () => {
-      this.renderDocument(true);
+      this.renderedList = [];
+      this.update();
     });
   }
 
@@ -176,33 +292,23 @@ export default class PDFRenderer extends React.Component<
       trigger('TOTAL_PAGES', {
         totalPages: pdf.numPages
       });
-      this.setState({ pdf }, () => {
-        for (let i = 1; i <= pdf.numPages; i++) {
-          this.pageSetup(i);
-        }
+      for (let i = 0; i < pdf.numPages; i++) {
+        this.state.pages.push({
+          pageNumber: i,
+          divRef: React.createRef(),
+          canvasRef: React.createRef(),
+          loaded: false
+        });
+      }
+      this.setState({
+        pages: this.state.pages,
+        pdf
       });
     });
   };
 
-  pageSetup = (index: number) => {
-    this.state.pdf.getPage(index).then((page) => {
-      const viewport = page.getViewport({ scale: this.props.scale });
-      this.state.pages.push({
-        page,
-        viewport,
-        pageNumber: index,
-        divRef: React.createRef(),
-        canvasRef: React.createRef(),
-        rendered: false
-      });
-      this.setState({
-        pages: this.state.pages
-      });
-    });
-  }
-
   buildPageDOM = (page: Page) => {
-    if (page.rendered) {
+    if (page.viewport && page.loaded) {
       return (
         <>
           <div
@@ -210,7 +316,9 @@ export default class PDFRenderer extends React.Component<
             id={`page-${page.pageNumber}`} 
             className="page-wrapper"
             style={{height: page.viewport.height, width: page.viewport.width}}
-          ></div>
+          >
+            <canvas ref={page.canvasRef} height={page.viewport.height} width={page.viewport.width}></canvas>
+          </div>
         </>
       );
     } else {
@@ -220,9 +328,8 @@ export default class PDFRenderer extends React.Component<
             ref={page.divRef}
             id={`page-${page.pageNumber}`} 
             className="page-wrapper"
-          >
-            <canvas ref={page.canvasRef} height={page.viewport.height} width={page.viewport.width}></canvas>
-          </div>
+            style={{height: DEFAULT_HEIGHT * this.props.scale, width: DEFAULT_WIDTH * this.props.scale}}
+          ></div>
         </>
       );
     }
@@ -235,7 +342,7 @@ export default class PDFRenderer extends React.Component<
     }
     return (
       <>
-        <div className="pdfViewer singlePageView">
+        <div className="pdfViewer">
           {items}
         </div>
       </>
